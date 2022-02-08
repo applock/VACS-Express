b48be93cee5365f717519131cd8bae613c09f615
Iman <ibiswas@adobe.com>
2022-02-08 18:00:13 +0530

    update logic

--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/v1/control/barrier", (req, resp) => {
+router.get("/v1/currentstate/barrier/:barrierId", (req, resp) => {
   // #swagger.tags = ['Barrier Controller']
-  // #swagger.path = '/v1/control/barrier'
-  // #swagger.description = 'Controls barrier gate'
-  resp.send({ status: "good" });
+  // #swagger.path = '/v1/currentstate/barrier/{barrierId}'
+  // #swagger.description = 'Get barrier gate open/closed/online status'
+  resp.send({ barrierId: req.params.barrierId, alive: true, state: "Closed" });
+});
+
+router.post("/v1/modifystate/barrier", (req, resp) => {
+  // #swagger.tags = ['Barrier Controller']
+  // #swagger.path = '/v1/modifystate/barrier'
+  // #swagger.description = 'Update barrier gate to open/closed status'
+  /*  #swagger.parameters['obj'] = {
+        in: 'body',
+        description: 'Schema for barrier control',
+        schema: {
+          "$barrierId": "",
+          "$state": "Open"
+        }
+    } */
+  resp.send({ barrierId: req.params.barrierId, alive: true, state: "Closed" });
 });
 
 module.exports = router;
